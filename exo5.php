f6e4fb490931b27fbdceda0f92d79ba5e8ca64bc
Stéphane <liangstephane88@gmail.com>
2022-02-11 12:21:06 +0100

    decroissant

---
+++ exo5.php
@@ -0,0 +1,19 @@
+<?php
+ for ($i=25;$i > 0; $i--) 
+ { 
+     echo $i.' ';
+ }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
