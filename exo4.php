cfb49f7de2a48e1d9ce301f884a71ddc4a1f37b6
Stéphane <liangstephane88@gmail.com>
2022-02-11 12:17:45 +0100

    croissant

--- exo4.php
+++ exo4.php
@@ -1,1 +1,19 @@
-"croissant25" 
+<?php
+ for ($i=0;$i < 26; $i++) 
+ { 
+     echo $i.' ';
+ }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
