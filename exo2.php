83c4212cfa2a51bf5054416e78a84cc9aba8472e
Stéphane <liangstephane88@gmail.com>
2022-02-11 11:47:46 +0100

    pyta

--- exo2.php
+++ exo2.php
@@ -1,1 +1,20 @@
-"Pytagore" 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<html>
+<body>
+
+    <?php
+    echo hypot(3,6) . "<br>";
+    ?>
+
+</body>
+</html>
+</body>
+</html>
